a14b28b1c58a20f2bdf432b120af0e34701d0b28
rammyinsurance <rameswar.rath@gmail.com>
2025-07-05 20:50:14 +0530

    delete all file

--- README.md
+++
@@ -1,3 +0,0 @@
-# QuantSignal
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/imsonup/QuantSignal)
--- src/utils/animations.ts
+++
@@ -1,7 +0,0 @@
-// Animation utility classes for consistent animations across components
-export const fadeInUp = "animate-[fadeInUp_0.5s_ease-out]";
-export const fadeIn = "animate-[fadeIn_0.5s_ease-out]";
-export const scaleIn = "animate-[scaleIn_0.3s_ease-out]";
-export const slideInRight = "animate-[slideInRight_0.5s_ease-out]";
-export const hoverScale = "hover:scale-105 transition-transform duration-300";
-export const hoverGlow = "hover:shadow-[0_0_30px_rgba(147,51,234,0.3)] transition-shadow duration-300";
